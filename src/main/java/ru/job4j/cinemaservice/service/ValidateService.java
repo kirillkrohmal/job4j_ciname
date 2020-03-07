f9ab4aecdeb58bcd4f4cd674374f3840f0a0228d
kirillkrohmal <krohmal_kirill@mail.ru>
2020-03-07 19:29:47 +0300

    change

---
+++ src/main/java/ru/job4j/cinemaservice/service/ValidateService.java
@@ -0,0 +1,33 @@
+package ru.job4j.cinemaservice.service;
+
+
+
+import ru.job4j.cinemaservice.model.Halls;
+import ru.job4j.cinemaservice.persistence.JdbcStore;
+
+import java.util.Collection;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class ValidateService {
+    private final JdbcStore logic = JdbcStore.getInstance();
+    private static final ValidateService INSTANCE = new ValidateService();
+    private ConcurrentHashMap<Integer, Halls> storage = new ConcurrentHashMap<>();
+
+    public Collection<Halls> values() {
+        return storage.values();
+    }
+
+    public static ValidateService getInstance() {
+        return INSTANCE;
+    }
+
+    public boolean addHalls(String rows, String place) {
+        boolean result = false;
+
+        if (rows != null && place != null ) {
+            logic.addHolls(new Halls(rows, place));
+        }
+
+        return result;
+    }
+}
