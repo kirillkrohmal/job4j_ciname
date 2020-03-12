e6dd71d68c3c3726cddd44ec694be0d8bffbc3f2
kirillkrohmal <krohmal_kirill@mail.ru>
2020-03-12 20:27:20 +0300

    Сервис-кинотеатр

--- src/main/java/ru/job4j/cinemaservice/model/Accounts.java
+++ src/main/java/ru/job4j/cinemaservice/model/Accounts.java
@@ -5,6 +5,8 @@ import java.util.Objects;
 public class Accounts {
     private int id;
     private String value;
+    private String username;
+    private int phone;
 
 
     public Accounts() {
@@ -31,18 +33,36 @@ public class Accounts {
         this.value = value;
     }
 
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public int getPhone() {
+        return phone;
+    }
+
+    public void setPhone(int phone) {
+        this.phone = phone;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Accounts accounts = (Accounts) o;
         return id == accounts.id &&
-                Objects.equals(value, accounts.value);
+                phone == accounts.phone &&
+                Objects.equals(value, accounts.value) &&
+                Objects.equals(username, accounts.username);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, value);
+        return Objects.hash(id, value, username, phone);
     }
 }
 
--- src/main/resources/cinema.sql
+++ src/main/resources/cinema.sql
@@ -1,15 +1,17 @@
 CREATE TABLE accounts
 (
     id serial NOT NULL,
+    value character varying(2000),
     username character varying(2000),
-    phone character varying(2000),
+    phone integer,
     CONSTRAINT accounts_pkey PRIMARY KEY (id)
 );
 
 CREATE TABLE halls
 (
-    id serial NOT NULL,
-    rows character varying(2000),
-    place character varying(2000),
-    CONSTRAINT halls_pkey PRIMARY KEY (id)
+  id serial NOT NULL,
+  rows character varying(2000),
+  columns character varying(2000),
+  account_id integer REFERENCES accounts (id)б
+  CONSTRAINT halls_pkey PRIMARY KEY (id)
 );
